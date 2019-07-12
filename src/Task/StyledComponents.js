55d10131c38a23455c25fba8d6a1d233b5dd7a7c
itsmeganesh-cse-iiit <itsmeganeshcse@gmail.com>
2019-07-12 20:02:38 +0530

    Task Completed

--- src/Task/StyledComponents.js
+++ src/Task/StyledComponents.js
@@ -1,35 +1,43 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-export const Heading=styled.div`
-margin:auto;
-width:50%;
-`
-export const EventsContainer=styled.div`
-margin: auto;
-width: 50%;
-padding: 10px;
-display: flex;
-justify-content: center;
-`
-export const DisplayContainer=styled.div`
-padding: 10px;
-`
+export const Heading = styled.div`
+  margin: auto;
+  width: 50%;
+`;
+export const EventsContainer = styled.div`
+  margin: auto;
+  width: 50%;
+  padding: 10px;
+  display: flex;
+  justify-content: center;
+`;
+export const DisplayContainer = styled.div`
+  padding: 10px;
+`;
 
-export const CountContainer=styled.div`
-padding:10px;
-`
+export const CountContainer = styled.div`
+  padding: 10px;
+`;
 
-export const CustomButton=styled.button`
-margin:5px;
-color:${props=>props.color};
-background-color:${props=>props.backgroundColor};
-`
+export const CustomButton = styled.button`
+  margin: 5px;
+  color: ${props => props.color};
+  background-color: ${props => props.backgroundColor};
+`;
 
-export const CountButton=styled.button`
-margin:5px;
-`
+export const CountBox = styled.span`
+  margin: 5px;
+  width: 10px;
+  color: white;
+  padding: 2px;
+  font-weight: bold;
+  ${props =>
+    props.borderTop &&
+    css`
+      border-top: 5px solid orange;
+    `}
+`;
 
-export const FontChanger=styled.span`
-font-size:${props=>props.fontSize}pt;
-font-family:"Arial, Helvetica, sans-serif";
-`
+export const FontChanger = styled.span`
+  font-size: ${props => props.fontSize}pt;
+`;
--- src/Task/Task.js
+++ src/Task/Task.js
@@ -5,61 +5,110 @@ import {
   DisplayContainer,
   CountContainer,
   CustomButton,
-  CountButton,
+  CountBox,
   FontChanger
 } from "./StyledComponents";
+
 class Task extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      counterArr: [1, 2, 3, 4, 5, 6],
-      countVal:10
+      counterArr: [],
+      countVal: 0
     };
   }
 
+  handleAddEvent = () => {
+    let newNumber = this.state.counterArr.length + 1;
+    let tmpArr = [...this.state.counterArr, newNumber];
+    this.setState({
+      counterArr: tmpArr,
+      countVal: newNumber
+    });
+  };
+  handleRemoveEvent = () => {
+    if (this.state.counterArr.length > 0) {
+      this.state.counterArr.pop();
+      let tmpArr = this.state.counterArr;
+      this.setState({
+        counterArr: tmpArr,
+        countVal: tmpArr.length
+      });
+    } else {
+      alert("No more boxes to remove..!!!");
+    }
+  };
 
-  handleAddEvent=()=>{
-    let newNumber=this.state.counterArr.length+1;
-    let tmpArr=[...this.state.counterArr,newNumber]
-    this.setState({
-        counterArr:tmpArr,
-        countVal:newNumber
-    },()=>{
-        console.log(this.state.counterArr)
-    })
+  handleEvent = event => {
+    switch (event) {
+      case "add":
+        this.handleAddEvent();
+        break;
+      case "remove":
+        this.handleRemoveEvent();
+        break;
+      default:
+        console.log("working in background");
+    }
+  };
+  checkIndexIsEven(n) {
+    return n % 2 === 0;
   }
-  handleRemoveEvent=()=>{   
-    this.state.counterArr.pop()
-    let tmpArr=this.state.counterArr
-    this.setState({
-        counterArr:tmpArr
-    },()=>{
-        console.log(this.state.counterArr)
-    })
-}
 
   render() {
+    const { counterArr, countVal } = this.state;
     return (
       <React.Fragment>
         <Heading>
           <h1>Start Building</h1>
         </Heading>
         <EventsContainer>
-          <CustomButton color="white" backgroundColor="green" onClick={this.handleAddEvent}>
+          <CustomButton
+            color="white"
+            backgroundColor="green"
+            onClick={() => this.handleEvent("add")}
+          >
             Add Box
           </CustomButton>
-          <CustomButton color="white" backgroundColor="red" onClick={this.handleRemoveEvent}>
+          <CustomButton
+            color="white"
+            backgroundColor="red"
+            onClick={() => this.handleEvent("remove")}
+          >
             Remove Box
           </CustomButton>
         </EventsContainer>
         <DisplayContainer>
-          {this.state.counterArr.map(eachBtn => (
-            <CountButton>{eachBtn}</CountButton>
-          ))}
+          {counterArr.map(eachBtn =>
+            eachBtn % 5 === 0 ? (
+              <CountBox
+                key={eachBtn}
+                borderTop="true"
+                style={{
+                  backgroundColor: this.checkIndexIsEven(eachBtn)
+                    ? "black"
+                    : "blue"
+                }}
+              >
+                {eachBtn}
+              </CountBox>
+            ) : (
+              <CountBox
+                key={eachBtn}
+                style={{
+                  backgroundColor: this.checkIndexIsEven(eachBtn)
+                    ? "black"
+                    : "blue"
+                }}
+              >
+                {eachBtn}
+              </CountBox>
+            )
+          )}
         </DisplayContainer>
         <CountContainer>
-        <FontChanger fontSize={this.state.countVal}>{this.state.countVal}</FontChanger>
+          <FontChanger fontSize={countVal}>{countVal}</FontChanger>
         </CountContainer>
       </React.Fragment>
     );
